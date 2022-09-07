bad4aedceecc16950e8896ac684dc01b8172ee58
Fatima Hardoy <fatima.hardoy@gmail.com>
2022-09-07 14:27:35 -0300

    styling category block component

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,17 @@
 import NavBar from "../components/NavBar.js";
 import Search from "../components/Search";
 import classnames from "classnames";
+import CategoriesBlock from "../components/CategoriesBlock.js";
 
 function Home() {
-  const container = classnames(" w-full md:w-1/2 flex items-center");
+  const container = classnames("w-full md:w-1/2 flex items-center justify-center");
   return (
     <div className="max-w-[1400px] ml-auto mr-auto">
       <div className="w-full overflow-x-hidden overflow-y-hidden">
         <NavBar />
       </div>
       <div className="flex flex-wrap-reverse md:flex-nowrap  h-[calc(100vh-216px)] gap-4 w-full px-4 md:px-12 md:py-6">
-        <div className={container}>Componente de bloque de categorias</div>
+        <div className={container}><CategoriesBlock/></div>
         <div className={container}>
           <Search />
         </div>
