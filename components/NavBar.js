c76148b38de6bd57f2ca6105208aabfc5084bb19
OFMA2022 <ayofe08@gmail.com>
2022-09-09 19:48:36 +0200

    Adding-Menu-Hamburguer

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,40 +1,27 @@
+import classnames from "classnames";
+import NavBarList from "./NavBarList";
+import NavBarMenuHamburger from "./NavBarMenuHamburger";
+import React, { useState } from "react";
+
 function navBar() {
   {
     /* CONSTANTES */
   }
+  const [open, setOpen] = useState(false);
+  const handleClick = () => {
+    setOpen(!open);
+  };
 
   return (
     <nav className="flex justify-between items-center p-4 md:px-12 md:py-6">
       <div className="font-satisfy text-logo px-1 logo font-bold">
         Good Hands
       </div>
-      {/* TODO: Add hamburguer menu in <sm */}
-      <div className="hidden sm:flex sm:gap-8 text-lg">
-        {/* TODO: transform this to a map. */}
-        <a
-          className="link link-underline link-underline-black text-black"
-          href=""
-        >
-          Ropa
-        </a>
-        <a
-          className="link link-underline link-underline-black text-black"
-          href=""
-        >
-          Juguetes
-        </a>
-        <a
-          className="link link-underline link-underline-black text-black"
-          href=""
-        >
-          Muebles
-        </a>
-        <a
-          className="link link-underline link-underline-black text-black"
-          href=""
-        >
-          Alimentos
-        </a>
+
+      <NavBarList open={open} />
+
+      <div className="sm:hidden text-black">
+        <NavBarMenuHamburger open={open} handleClick={handleClick} />
       </div>
     </nav>
   );
