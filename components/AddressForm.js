d95aeb7fb43075f73ad35d703a432c536d350997
OFMA2022 <ayofe08@gmail.com>
2022-09-12 19:55:31 +0200

    Add google map api

---
+++ components/AddressForm.js
@@ -0,0 +1,79 @@
+import React, { useState } from "react";
+import AutoComplete from "react-google-autocomplete";
+import { useFormikContext } from "formik";
+import { inputClass } from "components/Inputs/InputBase";
+import { Input } from "components/Inputs";
+
+const GOOGLE_API_KEY = process.env.NEXT_PUBLIC_GOOGLE_API_KEY;
+
+const AddressForm = ({ values, ACCOUNT_FIELDS }) => {
+  const { setFieldValue } = useFormikContext();
+  const [addressInputs, setStateAddressInputs] = useState(false);
+  const handleClick = () => setStateAddressInputs(!addressInputs);
+
+  const handlePlaceSelected = (place) => {
+    const route = place.address_components.find((a) =>
+      a.types.includes("route")
+    )?.long_name;
+    const street_number = place.address_components.find((a) =>
+      a.types.includes("street_number")
+    )?.long_name;
+    setFieldValue("business_address", route + " " + street_number);
+
+    const postal_code = place.address_components.find((a) =>
+      a.types.includes("postal_code")
+    )?.long_name;
+    setFieldValue("zip_code", postal_code);
+
+    const city = place.address_components.find((a) =>
+      a.types.includes("locality")
+    )?.long_name;
+    setFieldValue("city", city);
+
+    const province = place.address_components.find((a) =>
+      a.types.includes("administrative_area_level_1")
+    )?.long_name;
+    setFieldValue("province", province);
+
+    const country = place.address_components.find((a) =>
+      a.types.includes("country")
+    )?.long_name;
+    setFieldValue("country", country);
+
+    setFieldValue("google_id", place.place_id);
+    if (place?.geometry?.location) {
+      const lat = place.geometry.location.lat();
+      const lng = place.geometry.location.lng();
+      setFieldValue("latitude", lat);
+      setFieldValue("longitude", lng);
+    }
+  };
+  return (
+    <>
+      <div className="flex flex-col gap-2" ref={ref}>
+        <div>
+          <span className="block text-input leading-5 text-black mb-1">
+            Tu dirección
+          </span>
+
+          <AutoComplete
+            apiKey={GOOGLE_API_KEY}
+            defaultValue={
+              (values.business_address ? values.business_address + ", " : "") +
+              (values.city ? values.city + ", " : "") +
+              (values.province ? values.province + ", " : "") +
+              (values.country ? values.country : "")
+            }
+            onPlaceSelected={handlePlaceSelected}
+            className="rounded-lg px-2 py-1  w-full border border-solid border-gray-200  transition ease-in-out focus:border-input-border-focus focus:outline-none hover:border-input-border-hover focus:shadow-input-focus"
+            options={{
+              types: ["address"],
+            }}
+          />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default AddressForm;
--- components/Search.js
+++ components/Search.js
@@ -1,11 +1,22 @@
 import { useState } from "react";
 import Button from "../components/Button";
+import AutoComplete from "react-google-autocomplete";
+
+const GOOGLE_API_KEY = process.env.NEXT_PUBLIC_GOOGLE_API_KEY;
 
 function Search() {
   const [direction, setDirection] = useState();
+
+  const handlePlaceSelected = (place) => {
+    const address = place.address_components.types;
+    setDirection(place.geometry.location.lat());
+    console.log("setdirection", direction);
+    console.log("address", address);
+  };
+
   return (
-    <div className="flex flex-col gap-12 w-full items-center text-center">
-      <div className="text-5xl uppercase font-bold">
+    <div className="flex flex-col gap-5 sm:gap-12 w-full items-center text-center">
+      <div className="text-3xl  sm:text-5xl uppercase font-bold">
         ¿Quieres donar y no sabes dónde?
       </div>
       <div className="text-lg">
@@ -16,11 +27,13 @@ function Search() {
         onSubmit={(e) => setDirection(e.target.value)}
         className="flex justify-between gap-2 w-full h-full"
       >
-        <input
+        <AutoComplete
+          apiKey={GOOGLE_API_KEY}
+          onPlaceSelected={(place) => console.log(place)}
           className="rounded-lg px-2 py-1  w-full border border-solid border-gray-200  transition ease-in-out focus:border-input-border-focus focus:outline-none hover:border-input-border-hover focus:shadow-input-focus"
-          type="text"
-          value={direction}
-          placeholder="Pon tu dirección"
+          options={{
+            types: ["address"],
+          }}
         />
         <Button type="submit">Buscar</Button>
       </form>
